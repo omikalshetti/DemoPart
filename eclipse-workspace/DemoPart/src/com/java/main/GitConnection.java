d28335bd7922530872bd1a34c9d4584af2e66899
onkar <onkar@gmail.com>
2023-12-10 20:51:26 +0530

    First commit........

---
+++ eclipse-workspace/DemoPart/src/com/java/main/GitConnection.java
@@ -0,0 +1,14 @@
+package com.java.main;
+
+public class GitConnection {
+
+	public static void main(String[] args) {
+		
+		System.out.println("For git & git trial");
+		
+		for (int i=1; i<=5; i++)
+		{
+			System.out.println("Suceess");
+		}
+	}
+}
